284ddc30bf5d5898fe7a69f7ce1c900231cd0b4e
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-08-10 14:46:54 +0530

    Deleted Input file

--- input.py
+++
@@ -1,7 +0,0 @@
-"""
-xml_input = 'https://d31l2nn7dlh4li.cloudfront.net/amg02134/epg_deliveries/amgplt0001/amg02134c2/DonutYYYY-MM-DD.xml'
-#xml_input = 'https://d31l2nn7dlh4li.cloudfront.net/amg01927/epg_deliveries/amgplt0001/amg01927c1/amg01927c12026-08-05.xml'
-#xml_input = 'https://d31l2nn7dlh4li.cloudfront.net/amg35787/epg_deliveries/amgplt0002/amg35787c2/amg35787c2Today.xml'
-content_type = 'episode'
-Validation_Output = []
-"""
