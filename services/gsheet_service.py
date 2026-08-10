31b144369ca676c9071a35149e2fa8d8f22f066b
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-08-10 12:44:46 +0530

    Change date format to YYYY-MM-DD

--- services/gsheet_service.py
+++ services/gsheet_service.py
@@ -51,7 +51,7 @@ def validation_data():
 
     headers = worksheet.row_values(1)
     logger.info(f'Headers: {headers}')
-    today = datetime.now().strftime("%d-%b-%Y")
+    today = datetime.now().strftime("%Y-%m-%d")
     logger.info(f'Today Date: {today}')
     new_column_number = len(headers) + 1 if str(today) not in headers else len(headers)
     logger.info(f'New_Column_Number: {new_column_number}')
