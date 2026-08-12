102c645fc77c4b7c048e8e7466a0b0755e6895cb
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-08-12 12:39:15 +0530

    Updated Next Asset Start Time

--- utilities/child_template.py
+++ utilities/child_template.py
@@ -211,6 +211,7 @@ def validate_programs_seven_days_xml(date_xml_data, num, name, method_name, file
                                 if next_asset_time != start:
                                     failed_cases_3.append({asset_id: [date, start_time, int(next_asset_time)]})
 
+                            else:
                                 next_asset_time = stop
 
                         else:
