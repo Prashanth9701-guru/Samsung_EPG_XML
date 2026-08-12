bfd12e6458649a514e5ed0b0cf3745f937947175
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-08-12 12:07:18 +0530

    Refactor XML parsing and result handling logic

--- utilities/child_template.py
+++ utilities/child_template.py
@@ -160,29 +160,29 @@ def validate_programs_seven_days_xml(date_xml_data, num, name, method_name, file
 
     for single_date_xml_data in date_xml_data:
         for date, xml_data in single_date_xml_data.items():
-                root = ET.fromstring(xml_data)
-                programs = root.findall('programme')
-                if programs:
-                    if name not in ['Schedule']:
-                        results = method_name(programs, filed, channel_level_language, content_type, expected_length)
-                        logger.info(f'Results in Child File {filed} : {results}')
+            root = ET.fromstring(xml_data)
+            programs = root.findall('programme')
+            if programs:
+                if name not in ['Schedule']:
+                    results = method_name(programs, filed, channel_level_language, content_type, expected_length)
+                    logger.info(f'Results in Child File {filed} : {results}')
 
-                        if len(results) < 10:
-                            logger.info(f'Entered less than 10')
-                            for result, target_list in zip(results, lists):
-                                if result:
-                                    target_list.append({date : result})
-                        else:
-                            for result, target_list in zip(results, lists):
-                                if result:
-                                    target_list.append({date : result})
-                    elif name in ['Schedule']:
-                        for program in programs:
-                            start_time = program.attrib
-                            logger.info(f'Times of Asset: {start_time}')
+                    if len(results) < 10:
+                        logger.info(f'Entered less than 10')
+                        for result, target_list in zip(results, lists):
+                            if result:
+                                target_list.append({date : result})
+                    else:
+                        for result, target_list in zip(results, lists):
+                            if result:
+                                target_list.append({date : result})
+                elif name in ['Schedule']:
+                    for program in programs:
+                        start_time = program.attrib
+                        logger.info(f'Times of Asset: {start_time}')
 
-                else:
-                    programme_tag_availability.append({date: 'Programme Tag not available in XML'})
+            else:
+                programme_tag_availability.append({date: 'Programme Tag not available in XML'})
 
             #elif filed in ['Schedule']:
 
