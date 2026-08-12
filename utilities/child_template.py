5f53aa5a31fd41e2602164bc87c0401bb14b7e38
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-08-12 13:03:27 +0530

    Removed Next Asset Start Time

--- utilities/child_template.py
+++ utilities/child_template.py
@@ -209,7 +209,7 @@ def validate_programs_seven_days_xml(date_xml_data, num, name, method_name, file
 
                             if next_asset_time:
                                 if next_asset_time != start:
-                                    failed_cases_3.append({asset_id: [date, start_time, int(next_asset_time)]})
+                                    failed_cases_3.append({asset_id: [date, start_time, next_asset_time]})
 
                             else:
                                 next_asset_time = stop
