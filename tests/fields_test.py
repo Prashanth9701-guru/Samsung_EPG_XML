f94ccdbebbe74e627279452271284351416615b8
Prashanth9701-guru <guru.prashanth@amagi.com>
2026-09-23 17:26:13 +0530

    Enhance logging in fields_test.py
    Added logging for episode number and asset keys.

--- tests/fields_test.py
+++ tests/fields_test.py
@@ -57,6 +57,7 @@ def validate_asset_fields_availability(programs, man_fields, mand_child_values, 
             if 'episode' in str(effective_ct).lower():
                 asset_keys = list(program.keys())
                 logger.info(f'Asset Keys: {asset_keys}')
+                logger.info(f'Episode Num List: {program.get('episode-num')}')
                 episode_num = next(True for episode in program.get('episode-num') if episode.get('@system') in mand_child_values) if isinstance(program.get('episode-num'), list) else 'episode-num-onscreen'
                 logger.info(f'Episode Number: {episode_num}')
                 
